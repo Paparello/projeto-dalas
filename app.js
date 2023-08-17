78853125763edc087f6c2c9f4eab121d5fbbd818
José Roberto Paparello <paparello@gmail.com>
2023-08-17 11:17:41 -0300

    modularização

--- app.js
+++ app.js
@@ -1,30 +1,10 @@
+import capitalizar from './modificador.js';
+import ordenar from './modificador.js';
+
 let ingredientes = ['mel', 'água', 'sal', 'mostarda'];
 
-function capitalizar(ingredientes){
-    var modificado = [];
-
-    for(var i=0; i<ingredientes.length; i++){
-        var letraInicial = ingredientes[i].charAt(0).toUpperCase();
-        var restoTexto = ingredientes[i].slice(1); 
-        var resultado = letraInicial+restoTexto;
-        
-        modificado[i] = resultado;
-    }
-
-    return modificado;
-}   
-    //console.log(modificado); Não funciona, pois modificado não pode ser acessado de fora da função, então:
-    console.log(capitalizar(ingredientes)); //OK.
-
-function ordenar(ingredientes){
-
-    var ordenado = ingredientes.sort(function(a,b){
-        return a.localeCompare(b);   
-    });
-     return ordenado;
-} 
-    //Como a ordenação tem que ser no vetor já capitalizado, e não no vetor ingredientes, então:
     var resCapitalizado = capitalizar(ingredientes);
     var resOrdenado = ordenar(resCapitalizado);
+
     console.log(resCapitalizado);
     console.log(resOrdenado);
