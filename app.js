b1ef54af37490b9c312517663fabeb2adee2f2e5
José Roberto Paparello <paparello@gmail.com>
2023-08-17 18:54:32 -0300

    criei ingredientes.js

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 import modificador from './modificador.js';
+import ingredientes from './ingredientes.js';
 
-console.log(modificador);
+console.log(ingredientes);
+console.log(typeof ingredientes);
+console.log(Array.isArray(ingredientes));
 
-var ingredientes = ['mel', 'água', 'sal', 'mostarda'];
+/*var ingredientes = ['mel', 'água', 'sal', 'mostarda'];
 var resCapitalizado = modificador.capitalizar(ingredientes);
 var resOrdenado = modificador.ordenar(resCapitalizado);
 var resCaixaAlta = modificador.caixaAlta(resOrdenado);
 
 console.log(resCapitalizado);
 console.log(resOrdenado);
-console.log(resCaixaAlta);
+console.log(resCaixaAlta);*/
---
+++ ingredientes.js
@@ -0,0 +1,5 @@
+var ingredientes = [
+    {nome:'Alface', valor:2.5},{nome:'Tomate', valor:1},{nome:'Cebola',valor:0.7}
+]
+
+export default ingredientes;
